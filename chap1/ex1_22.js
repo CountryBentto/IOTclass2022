7009155ea45190603017f57a5c93b173a76f3850
Hojin Lee <ghwls512@cau.ac.kr>
2022-05-06 16:09:50 +0900

    // 예제 1-22 undefined와 null의 비교

---
+++ chap1/ex1_22.js
@@ -0,0 +1,10 @@
+var n = null;
+console.log(typeof n);
+ 
+console.log(n == undefined);
+console.log(n == null);
+
+console.log(n === undefined);   // null과 undefined는 ===으로 구분이 가능함
+console.log(n === null);
+
+// 예제 1-22 undefined와 null의 비교
