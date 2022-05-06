9bfc44725f37b925b558d9e245fb697de2161ac2
Hojin Lee <ghwls512@cau.ac.kr>
2022-05-06 16:09:29 +0900

    // 예제 1-20 undefined와 배열

---
+++ chap1/ex1_20.js
@@ -0,0 +1,16 @@
+var arr1 = [];
+arr1.length = 3;
+console.log(arr1);
+
+var arr2 = new Array(3);
+console.log(arr2);
+
+var arr3 = [undefined, undefined, undefined];
+console.log(arr3);
+
+// 예제 1-20 undefined와 배열
+
+// empty x 3 출력이 아닌
+// (3) […]
+// 사용 가능한 디버거가 없으므로 'variables'을(를) 보낼 수 없습니다.
+// 가 뜹니다...
