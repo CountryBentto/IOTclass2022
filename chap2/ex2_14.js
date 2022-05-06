0bb10127d2d62fbcdb87c92a7f5f0332981373e8
Hojin Lee <ghwls512@cau.ac.kr>
2022-05-06 16:12:12 +0900

    // 예제 2-14 스코프 체인 확인(크롬전용) // 해당 구문 및 크롬전용 코드구문은 실행하지 않았습니다.

---
+++ chap2/ex2_14.js
@@ -0,0 +1,12 @@
+var a = 1;
+var outer = function () {
+    var b = 2;
+    var inner = fucntion ()
+    {
+         console.dir(inner);
+    };
+    inner();
+};
+outer();
+
+// 예제 2-14 스코프 체인 확인(크롬전용) // 해당 구문 및 크롬전용 코드구문은 실행하지 않았습니다.
