334481d13cfcae1d8696b0946a97500c58dfbc0c
Hojin Lee <ghwls512@cau.ac.kr>
2022-05-06 16:11:29 +0900

    // 예제 2-10 함수 선언문과 함수 표현식 (2) - 호이스팅을 마친 상태

---
+++ chap2/ex2_10.js
@@ -0,0 +1,12 @@
+var sum = function sum(a, b){       // function sum (a, b)
+    return a + b;
+};
+var multiply;       //var multiply = function (a, b)
+console.log(sum(1,2));
+console.log(multiply(3,4));
+
+multiply =  function (a, b){
+    return a * b;
+};
+
+// 예제 2-10 함수 선언문과 함수 표현식 (2) - 호이스팅을 마친 상태
