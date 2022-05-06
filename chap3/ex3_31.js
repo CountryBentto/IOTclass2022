93de5d1ea08acda1d79a39730d3a35beae9d54db
Hojin Lee <ghwls512@cau.ac.kr>
2022-05-06 16:17:36 +0900

    // 예제 3-31 콜백 함수와 함께 thisArg를 받는 메서드

---
+++ chap3/ex3_31.js
@@ -0,0 +1,15 @@
+// Array.prototype.forEach(callback[, thisArg])
+// Array.prototype.map(callback[, thisArg])
+// Array.prototype.filter(callback[, thisArg])
+// Array.prototype.some(callback[, thisArg])
+// Array.prototype.every(callback[, thisArg])
+// Array.prototype.find(callback[, thisArg])
+// Array.prototype.finIndex(callback[, thisArg])
+// Array.prototype.flatMap(callback[, thisArg])
+// Array.prototype.from(arrayLike[, callback[, thisArg]])
+// Set.prototype.forEach(callback[, thisArg])
+// Map.prototype.forEach(callback[, thisArg])
+
+// 예제 3-31 콜백 함수와 함께 thisArg를 받는 메서드
+
+// 비실행예제
